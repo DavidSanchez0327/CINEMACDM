647a7335d7f020995f83eca5fff371fc26e3ebb4
David Mongui <cristhian.mongui@sofka.com.co>
2019-10-17 19:30:44 -0500

    ya valio

---
+++ CinemaCDM.Domain/PeliculaSeleccionada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaCDM.Domain
+{
+    public class PeliculaSeleccionada
+    {
+        public string Nombre { get; set; }
+        public string FechaEstreno { get; set; }
+        public string Imagen { get; set; }
+        public int Tiquetes { get; set; }
+        public double PrecioTotal { get; set; }
+        public Function FuncionSeleccionada { get; set; }
+    }
+}
